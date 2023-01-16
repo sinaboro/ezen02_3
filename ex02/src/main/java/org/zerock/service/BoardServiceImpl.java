d48e710527524e8ddda8cf0b0bfd7e1777c1a9df
free <rlaeocjf@gmail.com>
2023-01-16 15:32:11 +0900

    ex02-service

---
+++ ex02/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,44 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Service
+@AllArgsConstructor
+public class BoardServiceImpl implements BoardService{
+
+	private final BoardMapper boardMapper;
+		
+	@Override
+	public void register(BoardVO vo) {
+	   int result = boardMapper.insertSelectKey(vo);
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		return boardMapper.read(bno);
+	}
+
+	@Override
+	public boolean modify(BoardVO vo) {
+		return boardMapper.update(vo)==1 ? true: false;
+	}
+
+	@Override
+	public boolean remove(Long bno) {
+		return boardMapper.delete(bno) == 1? true:false;
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		return boardMapper.getList();
+	}
+
+}
---
+++ ex02/src/test/java/org/zerock/service/BoardServiceTests.java
@@ -0,0 +1,72 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapperTests;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardServiceTests {
+	
+	@Autowired
+	private BoardService boardService;
+	
+	@Test
+	public void testRegister() {
+		BoardVO vo =  new BoardVO();
+		//vo.setBno(null);
+		vo.setTitle("서비스 제목1");
+		vo.setContent("서비스 내용");
+		vo.setWriter("서비스1");
+		boardService.register(vo);
+	}
+	
+	@Test
+	public void testGet() {
+		BoardVO vo = boardService.get(7L);
+		log.info("vo======> " + vo);
+	}
+	
+	@Test
+	public void testModify() {
+		BoardVO vo = new BoardVO();
+		
+		vo.setBno(21L);
+		vo.setTitle("서비스 수정1");
+		vo.setContent("서비스 수정 내용1");
+		boolean bool = boardService.modify(vo);
+		log.info(bool);
+	}
+	
+	@Test
+	public void testDelete() {
+	   	boardService.remove(21L);
+	}
+	
+	@Test
+	public void testGetList() {
+//		List<BoardVO> boardVO= boardService.getList();
+//		boardVO.forEach(n->log.info(n));
+		
+		boardService.getList().forEach(n->log.info(n));
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
